6665e5d859dfaede78c31afdcd113c39069ef98d
Julien <julien-marchand@users.noreply.github.com>
2017-05-23 11:16:07 +0200

    Create experiments.py

---
+++ lessons/50-data-manipulation/52-Pandas/experiments.py
@@ -0,0 +1,19 @@
+raw_data = {
+        'subject_id': ['1', '2', '3', '4', '5'],
+        'first_name': ['Alex', 'Amy', 'Allen', 'Alice', 'Ayoung'],
+        'last_name': ['Anderson', 'Ackerman', 'Ali', 'Aoni', 'Atiches']}
+df_a = pd.DataFrame(raw_data, columns = ['subject_id', 'first_name', 'last_name'])
+df_a
+
+raw_data = {
+        'subject_id': ['4', '5', '6', '7', '8'],
+        'first_name': ['Billy', 'Brian', 'Bran', 'Bryce', 'Betty'],
+        'last_name': ['Bonder', 'Black', 'Balwner', 'Brice', 'Btisan']}
+df_b = pd.DataFrame(raw_data, columns = ['subject_id', 'first_name', 'last_name'])
+df_b
+
+raw_data = {
+        'subject_id': ['1', '2', '3', '4', '5', '7', '8', '9', '10', '11'],
+        'test_id': [51, 15, 15, 61, 16, 14, 15, 1, 61, 16]}
+df_n = pd.DataFrame(raw_data, columns = ['subject_id','test_id'])
+df_n
